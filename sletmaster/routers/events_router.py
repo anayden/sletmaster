d4bc2362ea6ac9c0165ba75c0f818b95c75f7fbd
Alexey Naiden <alexey.nayden@gmail.com>
2021-10-10 23:45:08 +0300

    Add events and locations APIs

--- sletmaster/routers/events_router.py
+++ sletmaster/routers/events_router.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Optional
 
+from beanie import PydanticObjectId
 from fastapi import APIRouter
 
 from sletmaster.models import Event
@@ -10,3 +11,32 @@ events_router = APIRouter()
 @events_router.get("/")
 async def list() -> List[Event]:
     return await Event.all(sort="start_time").to_list()
+
+
+@events_router.get("/{event_id}")
+async def get(event_id: PydanticObjectId) -> Optional[Event]:
+    event = await Event.get(event_id)
+    return event
+
+
+@events_router.post("/")
+async def create(event: Event) -> Event:
+    result = await Event.insert_one(event)
+    event.id = result.inserted_id
+    return event
+
+
+@events_router.put("/{event_id}")
+async def update(event_id: str, event: Event) -> Event:
+    if str(event.id) != event_id:
+        raise ValueError(f"event_id = {event_id}, event.id = {event.id}")
+    await event.save()
+    return event
+
+
+@events_router.delete("/{event_id}")
+async def delete(event_id: PydanticObjectId) -> None:
+    event = await Event.get(event_id)
+    if event is not None:
+        await event.delete()
+    return None
--- sletmaster/routers/locations_router.py
+++ sletmaster/routers/locations_router.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Optional
 
+from beanie import PydanticObjectId
 from fastapi import APIRouter
 
 from sletmaster.models import Location
@@ -10,3 +11,32 @@ locations_router = APIRouter()
 @locations_router.get("/")
 async def list() -> List[Location]:
     return await Location.all().to_list()
+
+
+@locations_router.get("/{location_id}")
+async def get(location_id: PydanticObjectId) -> Optional[Location]:
+    location = await Location.get(location_id)
+    return location
+
+
+@locations_router.post("/")
+async def create(location: Location) -> Location:
+    result = await Location.insert_one(location)
+    location.id = result.inserted_id
+    return location
+
+
+@locations_router.put("/{location_id}")
+async def update(location_id: str, location: Location) -> Location:
+    if str(location.id) != location_id:
+        raise ValueError(f"location_id = {location_id}, location.id = {location.id}")
+    await location.save()
+    return location
+
+
+@locations_router.delete("/{location_id}")
+async def delete(location_id: PydanticObjectId) -> None:
+    location = await Location.get(location_id)
+    if location is not None:
+        await location.delete()
+    return None
